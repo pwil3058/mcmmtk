6589507be4e5e6ccc0f11973be340c43a73c9f0f
Peter Williams <pwil3058@gmail.com>
2017-10-29 16:33:28 +1000

    rust: further development
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,91 @@
 extern crate gtk;
 extern crate gio;
+extern crate lazy_static;
 
 extern crate pw_gix;
 
 extern crate mcmmtk;
 
+use std::rc::Rc;
+
 use gio::ApplicationExt;
 
 use gtk::prelude::*;
 
-use pw_gix::gdkx::format_geometry;
 use pw_gix::colour;
+use pw_gix::colour::*;
+use pw_gix::colour::attributes::*;
+use pw_gix::gdkx::format_geometry;
+//use pw_gix::gtkx::entry;
+use pw_gix::gtkx::entry::*;
+use pw_gix::pwo::*;
 use pw_gix::rgb_math::rgb::*;
+use pw_gix::paint::*;
+use pw_gix::paint::components::*;
+use pw_gix::paint::model_paint::*;
+use pw_gix::paint::hue_wheel::*;
+use pw_gix::paint::target::*;
 
 use mcmmtk::recollections;
+
+trait ColourAttributeStackInterface {
+    type CASIType;
+    type PackableWidgetType;
+
+    fn create() -> Self::CASIType;
+    fn pwo(&self) -> Self::PackableWidgetType;
+}
+
+struct ColourAttributeStackData {
+    vbox: gtk::Box,
+    hcv_cads: HueChromaValueCADS,
+    rgb_entry_box: RGBHexEntryBox,
+    target_rgb_entry_box: RGBHexEntryBox,
+}
+
+type ColourAttributeStack = Rc<ColourAttributeStackData>;
+
+impl ColourAttributeStackInterface for ColourAttributeStack {
+    type CASIType = ColourAttributeStack;
+    type PackableWidgetType = gtk::Box;
+
+    fn create() -> ColourAttributeStack {
+        let vbox = gtk::Box::new(gtk::Orientation::Vertical, 0);
+        let hcv_cads = HueChromaValueCADS::create();
+        let rgb_entry_box = RGBHexEntryBox::create();
+        let target_rgb_entry_box = RGBHexEntryBox::create();
+        vbox.pack_start(&hcv_cads.pwo(), true, true, 0);
+        vbox.pack_start(&rgb_entry_box.pwo(), false, false, 0);
+        vbox.pack_start(&target_rgb_entry_box.pwo(), false, false, 0);
+        let cas = Rc::new(
+            ColourAttributeStackData {
+                vbox,
+                hcv_cads,
+                rgb_entry_box,
+                target_rgb_entry_box
+            }
+        );
+        let cas_c = cas.clone();
+        cas.rgb_entry_box.connect_value_changed(
+            move |rgb| {
+                let colour = colour::Colour::from(*rgb);
+                cas_c.hcv_cads.set_colour(Some(&colour));
+            }
+        );
+        let cas_c = cas.clone();
+        cas.target_rgb_entry_box.connect_value_changed(
+            move |rgb| {
+                let colour = colour::Colour::from(*rgb);
+                cas_c.hcv_cads.set_target_colour(Some(&colour));
+            }
+        );
+        cas
+    }
+
+    fn pwo(&self) -> gtk::Box {
+        self.vbox.clone()
+    }
+}
 
 fn activate(app: &gtk::Application) {
     let window = gtk::ApplicationWindow::new(app);
@@ -30,13 +102,22 @@ fn activate(app: &gtk::Application) {
         }
     );
     let vbox = gtk::Box::new(gtk::Orientation::Vertical, 0);
-    let value_indicator = colour::attributes::ValueCAD::new();
-    value_indicator.set_colour(Some(colour::Colour::from(BLUE)));
-    value_indicator.set_target_colour(Some(colour::Colour::from(YELLOW)));
-    //let snapshot_selector = g_snapshot::SnapshotSelector::new();
-    vbox.pack_start(&value_indicator.drawing_area, true, true, 0);
+    let colour_attribute_stack = ColourAttributeStack::create();
+    vbox.pack_start(&colour_attribute_stack.pwo(), true, true, 0);
+    let components_box = PaintComponentsBox::<ModelPaintCharacteristics>::create_with(6, true);
+    let ideal_paints = create_ideal_model_paint_series();
+    vbox.pack_start(&components_box.pwo(), false, true, 0);
+    let paint: ModelSeriesPaint = ideal_paints.get_series_paint("Red").unwrap();
+    components_box.add_paint(&Paint::Series(paint));
+    let wheel = PaintHueAttrWheel::<ModelPaintCharacteristics>::create(ScalarAttribute::Chroma);
+    wheel.pwo().show_all();
+    vbox.pack_start(&wheel.pwo(), true, true, 0);
+    wheel.add_paint(&ideal_paints.get_paint("Red").unwrap());
+    wheel.set_target_colour(Some(&Colour::from(YELLOW)));
+    let target_colour = TargetColour::create(&Colour::from(CYAN), "Cyan", "a very pure colour");
+    wheel.add_target_colour(&target_colour);
     let label = gtk::Label::new("GUI is under construction");
-    vbox.pack_start(&label, true, true, 0);
+    vbox.pack_start(&label, false, true, 0);
     window.add(&vbox);
     window.show_all();
 }
